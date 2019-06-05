bdc34a1618e5fa55de688cc163dd2cb506231997
chen jianjian <45163301+lumpop1@users.noreply.github.com>
2019-06-06 00:20:23 +0800

    Add files via upload

---
+++ app/build/generated/source/buildConfig/debug/com/jianjian/jc486415/memorygame/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.jianjian.jc486415.memorygame;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.a317468825.hw1.memorygame";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
